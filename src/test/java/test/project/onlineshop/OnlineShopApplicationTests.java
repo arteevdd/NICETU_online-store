4ae897773570dd9847e1032ed6e90bed2ba9198c
arteevdd <arteic4@yandex.ru>
2023-05-19 12:29:03 +0300

    Add flyway migration and docker-compose

--- src/test/java/test/project/onlineshop/OnlineShopApplicationTests.java
+++ src/test/java/test/project/onlineshop/OnlineShopApplicationTests.java
@@ -1,13 +1,9 @@
 package test.project.onlineshop;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OnlineShopApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
