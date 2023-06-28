46b3758d108da6a9af9c2fa9297419c0aa1ab38a
arteevdd <arteic4@yandex.ru>
2023-06-28 13:06:46 +0300

    Fix security config

--- src/main/java/test/project/onlineshop/dto/ProductListDto.java
+++ src/main/java/test/project/onlineshop/dto/ProductListDto.java
@@ -1,8 +1,12 @@
 package test.project.onlineshop.dto;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @Builder
+@Getter
+@Setter
 public class ProductListDto {
 
     private Integer productId;
