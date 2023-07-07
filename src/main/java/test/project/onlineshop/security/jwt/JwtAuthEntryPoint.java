2ec04c0176ea4de0259e3f5f905927d7968dee09
arteevdd <arteic4@yandex.ru>
2023-07-07 11:41:49 +0300

    Covered the Security layer with tests and write documentation for a tests

--- src/main/java/test/project/onlineshop/security/jwt/JwtAuthEntryPoint.java
+++ src/main/java/test/project/onlineshop/security/jwt/JwtAuthEntryPoint.java
@@ -3,14 +3,13 @@ package test.project.onlineshop.security.jwt;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
